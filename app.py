5d02dca5f6c25e4ada88a6d5fe38eec19dc2229d
LaFlameX07 <2021.amit.murkalmath@ves.ac.in>
2024-06-28 21:38:28 +0530

    second commit

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import io
-import base64
+import seaborn as sns
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 model = load_model('model/digit_recognition.h5')
+
+# Recompile the model after loading
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Load and preprocess the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -36,15 +40,21 @@ def index():
 def predict():
     if 'file' not in request.files:
         return redirect(request.url)
+    
     file = request.files['file']
+    
     if file.filename == '':
         return redirect(request.url)
+    
     if file:
-        img = load_img(file, color_mode='grayscale', target_size=(28, 28))
+        img_bytes = io.BytesIO(file.read())
+        img = load_img(img_bytes, color_mode='grayscale', target_size=(28, 28))
         img_array = img_to_array(img).reshape(1, 28, 28, 1).astype('float32') / 255
         prediction = model.predict(img_array)
         predicted_class = np.argmax(prediction)
+        image_path = os.path.join('uploads', file.filename)
         return render_template('result.html', predicted_class=predicted_class, image_path=file.filename)
+    
     return redirect(request.url)
 
 if __name__ == '__main__':
